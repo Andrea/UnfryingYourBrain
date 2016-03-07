422881cdcc75c6e8e0f1b1c2190aa4ab06448886
Roundcrisis <roundcrisis@gmail.com>
2016-03-07 10:48:26 +0000

    code samples

---
+++ QCon/InteropFSharp.fs
@@ -0,0 +1,22 @@
+﻿namespace QCon.InteropFSharp
+
+type Animal = {
+  Name: string
+  NumberOfLegs: int
+  NumberOfWings : int option
+  PreferedFood: string
+}
+
+type Order =
+  | GoldOrder 
+  | PlatinumOrder of string
+
+  member this.OrderInfo =
+    match this with 
+    | GoldOrder -> ""
+    | PlatinumOrder(extraInfo ) -> "A foamy latte"  + extraInfo
+
+type Customer =
+  | Dragon of string
+  | Cat 
+  | Monkey
---
+++ QConConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.FSharp.Core;
+using QCon.InteropFSharp;
+
+namespace QConConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciating an F# Record
+            var monkey = new Animal("Monkey", 4, FSharpOption<int>.None, "Bananas");
+
+
+            // Using a discriminated union
+            var platinumOrder = Order.NewPlatinumOrder( "Something");
+            Console.WriteLine("Is platinum? " +platinumOrder.IsPlatinumOrder);
+            Console.WriteLine("Extra info " +platinumOrder.OrderInfo);
+            
+            var goldOrder = Order.GoldOrder;
+            Console.WriteLine("Is platinum? " + goldOrder.IsPlatinumOrder);
+
+            //Another Discriminated uniong without the helper
+            var customer = Customer.NewDragon("Dragon Treats"); 
+            Console.WriteLine("Is a cat? " + customer.IsCat);
+            // no access to dragon customer info
+
+        }
+    }
+}
